2c5fc6145c7f7bc40e0a4c44e8878d2c3dc931fd
Myhailo Nyskohuz <mn508678@gmail.com>
2024-03-03 13:06:22 +0100

    Sequalize + env

--- backend/meest-calc-backend/db_connection.js
+++ backend/meest-calc-backend/db_connection.js
@@ -1,6 +1,17 @@
 const mysql = require('mysql2');
 const config = require('./config');
+const { Sequelize, DataTypes } = require('sequelize');
 
-const pool = mysql.createPool(config.database);
+// const pool = mysql.createPool(config.database);
 
-module.exports = pool;
+const sequelize = new Sequelize(process.env.DEVELOPMENT_DATABASE_NAME, process.env.DEVELOPMENT_DATABASE_USER, process.env.DEVELOPMENT_DATABASE_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DEVELOPMENT_DATABASE_HOST,
+    port: process.env.DEVELOPMENT_DATABASE_PORT,
+    waitForConnections: process.env.DEVELOPMENT_DATABASE_WAITFORCONNECTIONS === 'true', // Перетворення рядка в логічне значення
+    connectionLimit: parseInt(process.env.DEVELOPMENT_DATABASE_CONNECTIONLIMIT),
+    queueLimit: parseInt(process.env.DEVELOPMENT_DATABASE_QUEUELIMIT)
+});
+
+
+module.exports = sequelize;
